5810c76c87d5db0cfe2aa6e0a6abe12ee6a495c1
Ivan MSI <arigatory@gmail.com>
2022-06-11 05:56:25 +0300

    Add project files.

---
+++ src/TennisBookings/Configuration/FeaturesConfiguration.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TennisBookings.Configuration
+{
+	public class FeaturesConfiguration
+	{
+		public bool EnableWeatherForecast { get; set; }
+	}
+}
